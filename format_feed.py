1df1bf663d13bb4ac210831568ac2e695fea5f5f
Komaravolu <phani.komaravolu@cchmc.org>
2018-07-30 15:14:52 -0400

    remove explicit parser

--- format_feed.py
+++ format_feed.py
@@ -8,10 +8,10 @@ def format_feed_data(data, channel):
   return data
 
 def format_tech_meme_content(data):
-  return BeautifulSoup(data, 'html5lib').get_text()
+  return BeautifulSoup(data).get_text()
 
 def format_snook_content(data):
-  paragraph_data = BeautifulSoup(data, 'html5lib').find('p')
+  paragraph_data = BeautifulSoup(data).find('p')
   if paragraph_data:
     return paragraph_data.get_text()
   return ''
